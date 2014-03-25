dcd9afb9d5816146b00bb945f8cb083289f4d3db
Rachel Reese <rachel.reese@fireflylogic.com>
2014-03-24 21:40:41 -0400

    Oh hey, need a way to win.

--- Minesweeper/MinesweeperViewController.fs
+++ Minesweeper/MinesweeperViewController.fs
@@ -12,6 +12,7 @@ type MinesweeperViewController () =
     inherit UIViewController ()
 
     let mutable actionMode = Digging
+
     let NewSliderControl = 
         let s = new UISegmentedControl(new RectangleF((float32)50.f, (float32)Height*35.f+50.f, (float32)200.f, (float32)50.f))
         s.InsertSegment(UIImage.FromBundle("Flag.png"), 0, false)
@@ -45,11 +46,16 @@ type MinesweeperViewController () =
 
         let playNewGame (board:MinesweeperButton[,]) = 
             let v = new UIView(new RectangleF(0.f, 0.f, this.View.Bounds.Width, this.View.Bounds.Height))
-            board |> Array2D.map (fun b -> v.AddSubview b) 
+            board |> Array2D.iter (fun b -> v.AddSubview b) 
             this.View.AddSubview v
             this.View.BringSubviewToFront NewSliderControl
 
-        let rec MinesweeperButtonClicked = 
+        let rec MinesweeperButtonClicked =
+            let GameOver (view:UIView) heading text = 
+                view.RemoveFromSuperview()
+                (new UIAlertView(heading, text, null, "Okay", null)).Show()
+                playNewGame <| gameBoard()
+
             new EventHandler(fun sender eventargs -> 
                 let ms = sender :?> MinesweeperButton
                 let v = ms.Superview
@@ -60,14 +66,17 @@ type MinesweeperViewController () =
                         else
                             ms.SetImage(UIImage.FromBundle("Flag.png"), UIControlState.Normal)
                     | Digging when ms.IsMine -> //if you're digging, and you found a mine: death! :( 
-                        (new UIAlertView(":(", "YOU LOSE!", null, "Okay", null)).Show()
-                        v.RemoveFromSuperview()
-                        playNewGame <| gameBoard()
-                    | Digging -> // you're digging, clear the cell
+                        GameOver v ":(" "YOU LOSE!"
+                    | Digging -> // clear the cell
                         v.WillRemoveSubview(ms)
                         v.AddSubview <| NewUncoveredButton ms.SurroundingMines ms.Frame
-                        //todo: keep clearing all 0 cells
-                        //todo: if all non-mine cells are cleared, you win
+                        let msButtons = v.Subviews
+                                            |> Array.filter (fun o -> o.GetType().ToString().Contains("MinesweeperButton"))
+                                            |> Array.map (fun o -> o :?> MinesweeperButton)
+                                            |> Array.tryFind (fun o -> o.IsMine <> true)
+
+                        if (msButtons.IsNone) then
+                            GameOver v ":)" "YOU WIN!"
              )
 
         and gameBoard() = 
