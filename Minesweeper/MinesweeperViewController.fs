deaad79de9fdfa07a2e4a4199562d186ea28879c
Rachel Reese <rach@rachelree.se>
2014-10-09 23:23:20 -0400

    Segmented, not Slider.

--- Minesweeper/MinesweeperViewController.fs
+++ Minesweeper/MinesweeperViewController.fs
@@ -20,7 +20,7 @@ type MinesweeperViewController () =
  
     let mutable actionMode = Digging
 
-    /// Creates Slider Control
+    /// Creates Segmented Control
     let NewSliderControl =
         let s = new UISegmentedControl(new RectangleF((float32)50.f, (float32)Height*(ButtonSize+ButtonPadding)+50.f, (float32)200.f, (float32)50.f))
         s.InsertSegment(UIImage.FromBundle("Flag.png"), 0, false)
